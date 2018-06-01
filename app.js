52570ef01d586705929805321b0ca8a0751695bc
zahra-yousefi <z.yoosefi6@gmail.com>
2018-06-02 01:32:00 +0430

    added remove button

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
 var input = document.getElementById('newTask');
 var result = document.getElementById('result');
+
 var index = 1;
 function addItem() {
     var inputText = input.value;
     var listTag = document.createElement('div');
     var textNode = document.createTextNode(index + '. ' + inputText);
-    var spanTag = document.createElement('span');
-    spanTag.appendChild(textNode);
-    listTag.appendChild(spanTag);
+    var TextSpan = document.createElement('span');
+    TextSpan.appendChild(textNode);
+    listTag.appendChild(TextSpan);
     result.appendChild(listTag);
     input.value = '';
     index++;
+    var remove = document.createElement('button')
+    remove.textContent = 'delete';
+    
+    listTag.appendChild(remove);
+    remove.onclick = listTag.removeChild(listTag);
+ 
 }
