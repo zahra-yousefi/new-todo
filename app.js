e328ed278712a8abfa14c1c91426a301b5fc08ee
zahra-yousefi <z.yoosefi6@gmail.com>
2018-06-01 15:11:38 +0430

    refactored, added form and submit, added task index.

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
-function addItem(){
-    
-    var inputText = document.getElementById("newTask").value;
-    var listTag = document.createElement("listTag");
-    var textNode = document.createTextNode(inputText);
-    var spanTag = document.createElement("span");
+var input = document.getElementById('newTask');
+var result = document.getElementById('result');
+var index = 1;
+function addItem() {
+    var inputText = input.value;
+    var listTag = document.createElement('div');
+    var textNode = document.createTextNode(index + '. ' + inputText);
+    var spanTag = document.createElement('span');
     spanTag.appendChild(textNode);
     listTag.appendChild(spanTag);
-    var result = document.getElementById("result");
     result.appendChild(listTag);
-
+    input.value = '';
+    index++;
 }
