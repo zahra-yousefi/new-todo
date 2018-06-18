8196b0ddeb9dcaba0033fb5dcd346ac1e1e7d54f
zahra-yousefi <z.yoosefi6@gmail.com>
2018-06-18 21:49:32 +0430

    added do all, delete all, deleted list and todo list buutons.

--- app.js
+++ app.js
@@ -1,16 +1,47 @@
 var todoList = [];
 
 function addTodo(text) {
-    todoList.push({ text: text });
+    todoList.push({ text: text, isDeleted: false });
     render();
 }
 
 function removeTodo(todo) {
-    todoList.splice(todoList.indexOf(todo), 1);
+    todo.isDeleted = !todo.isDeleted;
     render();
 }
 
 function doUndoTodo(todo) {
+    if (todo.isDeleted) return;
     todo.isDone = !todo.isDone;
+    render();
+}
+
+function doAllTodos() {
+    for (var i = 0; i < todoList.length; i++) {
+        if(!todoList[i].isDeleted){
+            todoList[i].isDone = true;
+        }
+    }
+    render();
+}
+
+function deleteAllTodos() {
+    for (var i = 0; i < todoList.length; i++) {
+        if(!todoList[i].isDone){
+            todoList[i].isDeleted = true;
+        }
+    }
+    render();
+}
+
+function unDeleteAllTodos() {
+    for (var i = 0; i < todoList.length; i++) {
+        todoList[i].isDeleted = false;
+    }
+    render();
+}
+
+function showDeletedTodos() {
+    showDeleted = !showDeleted;
     render();
 }
--- view.js
+++ view.js
@@ -1,6 +1,17 @@
 var input = document.getElementById('newTask');
-var container = document.getElementById('todoListcontainer');
-var index = 1;
+var todoListContainer = document.getElementById('todoListcontainer');
+var showDeletedButton = document.getElementById('ShowDeleted');
+var doAllTodoButton = document.getElementById('doAllTodo');
+var deleteAllTodoButton = document.getElementById('deleteAllTodo');
+var unDeleteAllTodoButton = document.getElementById('unDeleteAllTodo');
+var noItemMassage = document.getElementById('noItemMassage');
+var headTitle = document.getElementById('head-title');
+var footer = document.getElementById('footer');
+var form = document.getElementById('form');
+
+var index;
+var showDeleted = false;
+var hasItem;
 
 function addItem() {
     var inputText = input.value;
@@ -13,28 +24,60 @@ function addItem() {
 }
 
 function render() {
-    index = 1;
-
+    dIndex = index = 0;
     clearContainer();
+    hasItem = false;
     for (var i = 0; i < todoList.length; i++) {
-        var li = createTodoElement(todoList[i]);
-        container.appendChild(li);
+        if (todoList[i].isDeleted == showDeleted) {
+            var li = createTodoElement(todoList[i]);
+            todoListContainer.appendChild(li);
+            hasItem = true;
+        }
+    }
+    handleStaticButtonsVisibility();
+}
+
+function handleStaticButtonsVisibility() {
+    if (hasItem) {
+        footer.style.display = 'block';
+        noItemMassage.style.display = 'none';
+    } else {
+        footer.style.display = 'none';
+        noItemMassage.style.display = 'block';
+    }
+
+    if (showDeleted) {
+        showDeletedButton.className = 'deleted-list-icon';
+        unDeleteAllTodoButton.style.display = 'inline';
+        doAllTodoButton.style.display = 'none';
+        deleteAllTodoButton.style.display = 'none';
+        form.style.display = 'none';
+        headTitle.innerText = 'Deleted List';
+    } else {
+        showDeletedButton.className = 'todo-list-icon';
+        unDeleteAllTodoButton.style.display = 'none';
+        doAllTodoButton.style.display = 'inline';
+        deleteAllTodoButton.style.display = 'inline';
+        form.style.display = 'inline';
+        headTitle.innerText = 'Todo List';
+
     }
 }
 
+
 function clearContainer() {
-    while (container.firstChild) {
-        container.removeChild(container.firstChild);
+    while (todoListContainer.firstChild) {
+        todoListContainer.removeChild(todoListContainer.firstChild);
     }
 }
 
 function createTodoElement(todo) {
-    var li = document.createElement('li');
-    li.className = 'li-task';
-    var span = document.createElement('span');
-    span.className = todo.isDone ? 'todo-undo-colore' : 'span-input';
-    span.textContent = index + '. ' + todo.text;
     index++;
+    var li = document.createElement('li');
+    var span = document.createElement('span');
+    span.textContent = index + '. ' + todo.text;
+    span.className = todo.isDone ? 'todo-undo-colore' : 'span-input';
+    li.className = 'li-task';
     li.appendChild(span);
 
     if (todo.isDone) {
@@ -43,14 +86,21 @@ function createTodoElement(todo) {
 
 
     var remove = document.createElement('span');
-    remove.className = 'delete-btn';
+    remove.className = todo.isDeleted ? 'undelete-btn' : 'delete-btn';
 
     var doUndo = document.createElement('span');
-    doUndo.className = todo.isDone ? 'undo-btn' : 'do-btn';
+    doUndo.className = (todo.isDone && !todo.isDeleted) ? 'undo-btn' : 'do-btn';
 
-    li.appendChild(remove);
-    remove.onclick = function () { removeTodo(todo); };
-    li.appendChild(doUndo);
+    if (!todo.isDone) {
+        li.appendChild(remove);
+    }
+    remove.onclick = function (e) {
+        e.stopPropagation();
+        removeTodo(todo);
+    };
+    if(!todo.isDeleted){
+        li.appendChild(doUndo);
+    }
     li.onclick = function () { doUndoTodo(todo); };
     return li;
 }
